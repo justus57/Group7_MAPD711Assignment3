7c744fc75b78a3953a8c1ea1e7faa221d5fdb73f
Justus <justuskasyoki57@gmail.com>
2022-11-16 06:07:09 +0300

    5nd commit

--- app/src/main/java/com/example/group7_mapd711assignment3/MapsActivity.kt
+++ app/src/main/java/com/example/group7_mapd711assignment3/MapsActivity.kt
@@ -93,9 +93,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
